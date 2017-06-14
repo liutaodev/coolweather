4778728ffd4bec7818232055e81832e5d992323e
LiuTao <530590887@qq.com>
2017-06-14 14:47:16 +0800

    完成显示选定城市天气的功能

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by 53059 on 2017/6/13.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
---
+++ app/src/main/java/com/coolweather/android/gson/Basic.java
@@ -0,0 +1,20 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by 53059 on 2017/6/13.
+ */
+
+public class Basic {
+    @SerializedName("city")
+    public String cityName;
+
+    @SerializedName("id")
+    public String weatherId;
+    public Update update;
+    public class Update{
+        @SerializedName("loc")
+        public String updateTime;
+    }
+}
